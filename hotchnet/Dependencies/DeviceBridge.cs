a96ecdd1e8f2fbc192a06b05968657a7fb741e9b
LochieR <lochie.rickerby@gmail.com>
2025-09-13 21:17:14 +0100

    initial commit

---
+++ hotchnet/Dependencies/DeviceBridge.cs
@@ -0,0 +1,50 @@
+﻿using System.Device.Gpio;
+
+namespace hotchnet.Dependencies;
+
+public class DeviceBridge : IDisposable
+{
+    private const int ControlLED = 17;
+    private const int ControlMotor = 18;
+    private GpioController Controller;
+
+    public bool LED
+    {
+        get
+        {
+            return Controller.Read(ControlLED) == PinValue.High;
+        }
+        set
+        {
+            Controller.Write(ControlLED, value == true ? PinValue.High : PinValue.Low);
+        }
+    }
+
+    public bool Motor
+    {
+        get
+        {
+            return Controller.Read(ControlMotor) == PinValue.High;
+        }
+        set
+        {
+            Controller.Write(ControlMotor, value == true ? PinValue.High : PinValue.Low);
+        }
+    }
+
+    public DeviceBridge()
+    {
+        Controller = new GpioController();
+        Controller.OpenPin(ControlLED, PinMode.Output, PinValue.Low);
+        Controller.OpenPin(ControlMotor, PinMode.Output, PinValue.Low);
+    }
+
+    public void Dispose()
+    {
+        Controller.ClosePin(ControlMotor);
+        Controller.ClosePin(ControlLED);
+        Controller.Dispose();
+
+        GC.SuppressFinalize(this);
+    }
+}
