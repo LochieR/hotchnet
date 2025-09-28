a6c1bd4c5a144d3df2c666b63e642ee2a2798175
LochieR <lochie.rickerby@gmail.com>
2025-09-28 18:20:03 +0100

    add timer over UART functionality

--- hotchnet/Dependencies/DeviceBridge.cs
+++ hotchnet/Dependencies/DeviceBridge.cs
@@ -1,7 +1,15 @@
 ﻿using System.IO.Ports;
 using System.Device.Gpio;
+using System.Runtime.InteropServices;
 
 namespace hotchnet.Dependencies;
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+file struct TimingData
+{
+    public int TimeMS;
+    public int Function;
+}
 
 public class DeviceBridge : IDisposable
 {
@@ -9,6 +17,9 @@ public class DeviceBridge : IDisposable
     private const int ControlMotor = 27;
     private GpioController Controller;
     private SerialPort SerialPort;
+
+    public const int FUNCTION_LED_ON = 0;
+    public const int FUNCTION_LED_OFF = 1;
 
     public bool LED
     {
@@ -46,6 +57,21 @@ public class DeviceBridge : IDisposable
             WriteTimeout = 500
         };
         SerialPort.Open();
+    }
+
+    public void ExecuteTimer(int timeMS, int function)
+    {
+        TimingData data = new()
+        {
+            TimeMS = timeMS,
+            Function = function
+        };
+
+        byte[] bytes = new byte[Marshal.SizeOf<TimingData>()];
+        Span<byte> span = bytes.AsSpan();
+        MemoryMarshal.Write(span, in data);
+
+        SerialPort.Write(bytes, 0, Marshal.SizeOf<TimingData>());
     }
 
     public void Dispose()
